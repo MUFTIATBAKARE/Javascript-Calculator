4f3bb4496fb243b2459692625ec6d3a89ca20d08
muftiatbakare <bakare.muftiat@gmail.com>
2022-06-23 00:07:30 -0700

    basic calculator using javascript

---
+++ script.js
@@ -0,0 +1,20 @@
+var opr = prompt("operator: ")
+var number1 = parseInt(prompt("Number 1: "))
+var number2 = parseInt(prompt("Number 2: "))
+
+if(opr == "+")
+{
+    console.log(number1 + number2)
+}
+else if(opr == "-")
+{
+    console.log(number1 - number2)
+}
+else if(opr == "*")
+{
+    console.log(number1 * number2)
+}
+else if(opr == "/")
+{
+    console.log(number1 / number2)
+}
